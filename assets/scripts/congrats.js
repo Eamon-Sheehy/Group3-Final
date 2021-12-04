019d943d08818f7150dc36c65ba19737f7717eb4
Nickolas <91910852+nickolasram@users.noreply.github.com>
2021-12-04 13:29:51 -0800

    Update congrats.js

--- assets/scripts/congrats.js
+++ assets/scripts/congrats.js
@@ -1,16 +1,13 @@
-let frame = 1;
-function congrats(){
-  let output = "";
-  let danceMan = "";
-  if (frame % 2 == 0) {
-    danceMan = "┏(^o^)┛";
-  }
+let frame = 1
+function congrats () {
+  let danceMan = ''
+  if (frame % 2 === 0) {
+    danceMan = '┏(^o^)┛' }
   else {
-    danceMan = "┗(^o^)┓";
-  }
-  document.getElementById("congrats").innerHTML = danceMan + "Congratulations! You have successfully set up your own public server through AWS!" + danceMan;
-  frame += 1;
+    danceMan = '┗(^o^)┓' }
+  document.getElementById('congrats').innerHTML = danceMan + 'Congratulations! You have successfully set up your own public server through AWS!' + danceMan;
+  frame += 1
 }
 function write(){
-  setInterval(function(){ congrats() }, 300);
+  setInterval(function () { congrats() }, 300)
 }
