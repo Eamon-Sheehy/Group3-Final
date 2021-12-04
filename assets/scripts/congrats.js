98525cce229f3323acaf09fa026daf90c1b42d84
Nickolas <91910852+nickolasram@users.noreply.github.com>
2021-12-04 13:40:32 -0800

    Update congrats.js

--- assets/scripts/congrats.js
+++ assets/scripts/congrats.js
@@ -2,9 +2,9 @@ let frame = 1
 function congrats () {
   let danceMan = ''
   if (frame % 2 === 0) {
-    danceMan = '┏(^o^)┛' 
+    danceMan = '┏(^o^)┛'
   } else {
-    danceMan = '┗(^o^)┓' 
+    danceMan = '┗(^o^)┓'
   }
   document.getElementById('congrats').innerHTML = danceMan + 'Congratulations! You have successfully set up your own public server through AWS!' + danceMan
   frame += 1
